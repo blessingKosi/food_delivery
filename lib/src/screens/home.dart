266f8efd674e94b6da3f57aa245869f5017fee8a
blessingKosi <kaniewu@gmail.com>
2021-02-15 18:08:54 +0000

    The firebase branch

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -142,7 +142,7 @@ class _HomeState extends State<Home> {
                                     size: 20,
                                   ),
                                 ),
-                                Text('4.5'),
+                                Text('4.6'),
                               ],
                             ),
                           ),
